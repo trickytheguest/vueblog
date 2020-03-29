d8a4322dc86de38a253c926f3d2fc12a8d40cc80
mzh <mzh.whut@gmail.com>
2020-03-29 23:57:02 +0800

    remove do not use shell file

--- deploy 2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd $(dirname $0)
-pwd
-git pull
-cd myblog/
-if [ -d "/var/www/html/vueblog/" ]; then
-    yarn docs:build && \cp -rf docs/.vuepress/dist/* /var/www/html/vueblog/
-fi
-systemctl restart httpd
