27835c5fd3f86bb2e20c0fbd5c3f8e3a47513fa4
Zhaohui <mzh.whut@gmail.com>
2020-04-06 23:25:07 +0800

    copy secure file to project config folder

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 cd $(dirname $0)
 pwd
 git pull
+# copy the secure file that the content is yourself info to project config folder
+\cp -rf ~/secureinfo.js myblog/config/
 cd myblog/
 if [ -d "/var/www/html/vueblog/" ]; then
     yarn docs:build && \cp -rf docs/.vuepress/dist/* /var/www/html/vueblog/
